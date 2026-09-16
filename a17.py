09417de18ccf432e8089770af2d002bde64494f7
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-09-16 14:54:07 +0530

    Create a17.py

---
+++ a17.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+a = pd.DataFrame([['sapna',56,16],['anmol',67,18],['rishul',75,16],['sameep',76,19]], index = [101,102,103,104], columns = ['name','agg','age'])
+print(a)
