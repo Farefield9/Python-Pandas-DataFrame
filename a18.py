cb21e0d9e2c0a4bcced93d67fdefc266c7b9daed
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-09-17 22:33:14 +0530

    Create a18.py

---
+++ a18.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+a = pd.DataFrame({'name':['sapna','anmol','rishul','sameep'], 'agg':[56,67,75,76], 'age':[16,18,16,19]}, index = [101,102,103,104])
+print(a)
