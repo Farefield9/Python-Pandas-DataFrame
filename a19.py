b345cfdcd5e6ed75f86120107aef8df79cd12225
Mannan Bhambri <mannanbhambri297@gmail.com>
2026-09-18 23:00:39 +0530

    Add initial DataFrame creation in a19.py

---
+++ a19.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+a = pd.DataFrame([{'name':'sapna', 'agg':56, 'age':16},{'name':'anmol', 'agg':67, 'age':18},{'name':'rishul', 'agg':75, 'age':16},{'name':'sameep', 'agg':76, 'age':19}])
+print(a)
